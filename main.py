0caedfbfcfc6a829305757d1a6ba04efcaac2a54
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-05-13 11:46:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def hello(first_name="GitHub", last_name="Actions"):
     if not isinstance(first_name, str) or not isinstance(last_name, str):
         raise TypeError("Both first_name and last_name must be strings")
-    return f"Hello, {first_name} {last_name}!"    
+    return f"Hello, {first_name} {last_name}!"
